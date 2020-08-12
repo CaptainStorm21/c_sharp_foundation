0a02a64cca6d7bbdf1d8bdb75a823d9bb9c4bc44
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-12 00:49:08 -0700

    data type vars

---
+++ 2-variables_data-types/2-variables_data-types/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2_variables_data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "This is a variable";
+            var bool_1 = true;
+            Console.WriteLine(a);
+            Console.WriteLine(bool_1);
+        }
+    }
+}
