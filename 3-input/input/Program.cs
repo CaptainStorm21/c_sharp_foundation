0c984977b720fc275a2a9531553b99fae5ff5e87
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-13 16:42:36 -0700

    update and new input tutorial

---
+++ 3-input/input/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            var anything = Console.ReadLine();
+            Console.WriteLine(anything);
+        }
+    }
+}
