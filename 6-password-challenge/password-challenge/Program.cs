1ac2018f2f61fbec3d2b5a902ee1a623ee316d6f
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-13 21:17:40 -0700

    passcode challenge

---
+++ 6-password-challenge/password-challenge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace password_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the password");
+            var passcode = Console.ReadLine();
+
+            if (passcode == "secret")
+            {
+                Console.WriteLine("Your password was accepted");
+            } else if (passcode != "secret")
+            {
+                Console.WriteLine("Your password was not accepted!");
+            }
+        }
+    }
+}
