12a73ac984bd86442342671698e4cec7c8a061f9
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-13 17:00:52 -0700

    input output challenge

---
+++ 4-input-output-survey/4-input-output-survey/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _4_input_output_survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("Where do you live?");
+            var city = Console.ReadLine();
+
+            Console.Write("Your name is : {0}", name + "\n");
+            Console.WriteLine("You live in {0}", city);
+            Console.WriteLine("You are {0}", age);
+        }
+    }
+}
