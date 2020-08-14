46f65354b6babf536a24ad012e1b06f46676b648
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-13 21:43:00 -0700

    while loop

---
+++ 7-while-password-challenge/password-challenge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace password_challenge
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var passcode = "";
+            while (passcode != "secret")
+            {
+                Console.WriteLine("What is the password?");
+                passcode = Console.ReadLine();
+
+                if (passcode != "secret")
+                {
+                    Console.WriteLine("Your password was not accepted!");
+                }
+            }
+            Console.WriteLine("Authenticated!");
+        }
+    }
+}
