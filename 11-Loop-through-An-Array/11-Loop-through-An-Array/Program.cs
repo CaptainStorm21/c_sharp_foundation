18853209ea08967d5d021c7c2903f457865238fc
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-14 02:18:24 -0700

    loop challenge

---
+++ 11-Loop-through-An-Array/11-Loop-through-An-Array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _11_Loop_through_An_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new is to make a new instance
+            var studentNames = new string[] { "Alex", "Mary", "Marina", "Polly", "Walter" };
+
+
+            foreach (var item in studentNames)
+            {
+                Console.WriteLine("Student in the class is " + item);
+            }
+        }
+    }
+}
