6516f19e02f448212498eda2b794a958b416c5cd
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-14 01:22:13 -0700

    array

---
+++ 10_Arrays/10_Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace _10_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // an array for students
+            var studentsGrades = new int[10];
+            studentsGrades[0] = 10;
+            studentsGrades[1] = 20;
+            studentsGrades[5] = 60;
+
+            Console.WriteLine("Student #5's score was " + studentsGrades[5]);
+
+            var studentIDs = new int[5] { 30, 31, 32, 33, 34 };
+            Console.WriteLine("Student Id is " + studentIDs[2]);
+
+            var studentNames = new string[] { "Alex", "Mary", "Marina", "Polly", "Walter"};
+            Console.WriteLine("Student's name is " + studentNames[2]);
+        }
+    }
+}
