e6c5e3000d9dd0aa2b503b09c380116a9aafb86b
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-14 00:59:18 -0700

    double for loop

---
+++ 9-double-for-loop-challenge/for-loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace for_loop
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // count from 1 to 10 and then back repeat 5 times
+            // cannot use if
+
+            for (int i_outerloop = 1; i_outerloop <= 5; i_outerloop++)
+            {
+                Console.WriteLine("Number of loop: " + i_outerloop);
+                for (int i = 0; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                for (int i = 10; i >= 1; i--)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
