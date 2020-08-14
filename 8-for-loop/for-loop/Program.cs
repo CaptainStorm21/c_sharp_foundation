8de6ca424686fec20944bb0296c06bd2f4d442c1
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-13 22:16:44 -0700

    for loop

---
+++ 8-for-loop/for-loop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace for_loop
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //var count = 1; 
+
+            for (int count = 1; count <=10; count++)
+            {
+                Console.WriteLine(count);
+            }
+
+            /*
+            while (count <= 10)
+            {
+                Console.WriteLine(count);
+                count++;
+            }
+            */
+        }
+    }
+}
