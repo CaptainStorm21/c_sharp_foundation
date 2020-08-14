9919518f39e9ac3346d8f9daf21f54c536473d78
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-13 18:03:52 -0700

    if statement

---
+++ 4-Conditions/Conditions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Conditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What month were you born in?");
+            var month = Console.ReadLine();
+
+            if (month == "march")
+            {
+                Console.WriteLine("you are an Aries");
+            }
+            else if (month == "November")
+             {
+                Console.WriteLine("you are Scorpio");
+            }
+           else if (month == "January")
+            {
+                Console.WriteLine("you are Capricorn");
+            }
+            else
+            {
+                Console.WriteLine("your zodiac doesnt exist");
+            }
+        }
+    }
+}
