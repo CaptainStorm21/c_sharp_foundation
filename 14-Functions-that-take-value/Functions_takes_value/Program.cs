8ba7277b82d9b7c416af4bd58e1f5cf3cff4da06
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-19 18:01:08 -0700

    function that take value

---
+++ 14-Functions-that-take-value/Functions_takes_value/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Functions_takes_value
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("What is your name?");
+                var name = TryAnswer();
+
+                Console.WriteLine("What is your age?");
+                var age =  TryAnswer();
+
+                Console.WriteLine("What pet do you have??");
+                var pet = TryAnswer();
+
+                Console.WriteLine("your name is {0}", name);
+                Console.WriteLine("your age is {0}", age);
+                Console.WriteLine("you have {0}", pet);
+
+                if (pet == "dog")
+                {
+                    Console.WriteLine("You are a dog person");
+
+                }
+                /*
+
+                if (age < 0 || age > 100)
+                {
+                    Console.WriteLine("Your entry was not valid");
+                }
+                */
+            }
+
+
+            static string TryAnswer()
+            {
+                var answer = Console.ReadLine();
+
+                if (answer == "")
+                {
+                    Console.WriteLine("You didn't enter any values...");
+                    return Console.ReadLine();
+                }
+                return answer;
+            }
+        }
+    }
+}
