c21eda9fa26a3c48230b19aa4f880d6488df62ca
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-23 23:04:18 -0700

    collections

---
+++ 17-Collections/17-Collections/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _17_Collections
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            var studentNames = new List<string>();
+            var studentGrades = new List<int>();
+
+            var adding = true;
+
+
+            while (adding)
+            {
+                Console.WriteLine("Student Name: ");
+                studentNames.Add(Console.ReadLine());
+
+                Console.WriteLine("Student Grades: ");
+                studentGrades.Add(int.Parse(Console.ReadLine()));
+
+                Console.WriteLine("Add Another Student? Yes/No");
+
+                if (Console.ReadLine() != "y")
+                {
+                    adding = false;
+                }
+
+
+            }
+
+
+            for (int i = 0; i < studentNames.Count; i++)
+            {
+                Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
+            }
+
+        }
+    }
+}
